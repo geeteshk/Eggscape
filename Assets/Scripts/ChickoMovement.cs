920cb9e59b7058ce03a152eb53bbdf72d18a215e
Geetesh Kalakoti <kalakotig@gmail.com>
2019-04-12 21:24:11 -0500

    Added blend trees for idle and walking

--- Assets/Scripts/ChickoMovement.cs
+++ Assets/Scripts/ChickoMovement.cs
@@ -10,23 +10,43 @@ public class ChickoMovement : MonoBehaviour
     // RigidBody2D of Chicko object
     private Rigidbody2D rigidBody;
 
+    // Animator of Chicko object
+    private Animator animator;
+
     // Difference the player moves by
     private Vector3 movementDelta;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     
     void Update()
     {
+        // Get raw input from axes so that movement is more snappy
         movementDelta = Vector3.zero;
         movementDelta.x = Input.GetAxisRaw("Horizontal");
         movementDelta.y = Input.GetAxisRaw("Vertical");
 
+        AnimateAndMoveChicko();
+    }
+
+    void AnimateAndMoveChicko()
+    {
         if (movementDelta != Vector3.zero)
         {
+            // Perform movement
             MoveChicko();
+
+            // Update animator values
+            animator.SetFloat("moveX", movementDelta.x);
+            animator.SetFloat("moveY", movementDelta.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
         }
     }
     
